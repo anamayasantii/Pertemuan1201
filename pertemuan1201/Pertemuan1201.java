dc0bdbe61cb2b460e8348baf44a0f331d19e9180
anamayasantii <111354009+anamayasantii@users.noreply.github.com>
2024-05-06 12:16:08 +0800

    Add files via upload

---
+++ pertemuan1201/Pertemuan1201.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan1201;
+
+/**
+ *
+ * @author 2201010156_Anamaya Santi
+ * 06 Mei 2024
+ */
+public class Pertemuan1201 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new jForm1().setVisible(true);
+        jForm1 A1 = new jForm1();
+        A1.setVisible(true);
+    }
+    
+}
